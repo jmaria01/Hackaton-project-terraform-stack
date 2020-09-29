73b60ed6ac49b12e63c17c33600329d87e6a02e5
Ubuntu <ubuntu@ip-172-31-46-6.ec2.internal>
2020-09-29 17:29:41 +0000

    changed web server count

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   ami           = "${lookup(var.aws_amis, var.aws_region)}"
 
-  count = 2
+  count = 1
 
   subnet_id              = "${random_shuffle.random_subnet.result[0]}"
   vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
